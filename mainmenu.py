bcf29bd5fe5d618ff6779b6b9e6a686c7388d435
Zeytax <alexflam2003@gmail.com>
2023-01-15 12:25:19 +0100

    buttons added

--- mainmenu.py
+++ mainmenu.py
@@ -1,38 +1,56 @@
 import pygame
+from utilities import Button
+
+def main():
+
+    pygame.mixer.pre_init(44100, -16, 2, 4096)
+    pygame.init()
+
+    x_screen_size = pygame.display.Info().current_w
+    y_screen_size = pygame.display.Info().current_h
+    
+    screen = pygame.display.set_mode((x_screen_size, y_screen_size))
+    #screen = pygame.display.set_mode((1000,1000))
+    pygame.display.set_caption('The game')
+
+    x_screen_size = pygame.display.Info().current_w
+    y_screen_size = pygame.display.Info().current_h
+
+    sound = pygame.mixer.Sound('music.mp3')
+    sound.play(-1)
+
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((0, 0, 0)) 
+
+    img = pygame.image.load("assets/play_button.png").convert_alpha()
+    playb = Button(x_screen_size * 0.5, y_screen_size * 0.4, img, y_screen_size / 500 )
+
+    img = pygame.image.load("assets/settings_button.png").convert_alpha()
+    settingsb = Button(x_screen_size * 0.5, y_screen_size * 0.6, img, y_screen_size / 500 )
+
+    img = pygame.image.load("assets/exit_button.png").convert_alpha()
+    exitb = Button(x_screen_size * 0.5, y_screen_size * 0.8, img, y_screen_size / 500 )
+
+    
+    while True:
+
+        if playb.draw(screen):
+            print("START")
+        
+        if settingsb.draw(screen):
+            print("SETTINGS")
+        
+        if exitb.draw(screen):
+            pygame.quit()
 
 
-
-class Menu():
-
-    def __init__(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
 
 
+        pygame.display.update()
 
 
-
-
-
-
-
-
-pygame.mixer.pre_init(44100, -16, 2, 4096)
-pygame.init()
-
-# screen = pygame.display.set_mode((pygame.display.Info().current_w, pygame.display.Info().current_h))
-screen = pygame.display.set_mode((1000,800))
-pygame.display.set_caption('The game')
-
-
-# Create The Backgound
-background = pygame.Surface(screen.get_size())
-background = background.convert()
-background.fill((000, 000, 000)) 
-
-sound = pygame.mixer.Sound('music.mp3')
-sound.play(-1)
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
+main()
