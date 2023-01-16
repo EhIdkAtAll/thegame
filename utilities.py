d8868b6186d2fca8a38a94da95366c2b4d7a2576
Zeytax <alexflam2003@gmail.com>
2023-01-16 19:23:13 +0100

    Added CheckBox class

--- utilities.py
+++ utilities.py
@@ -1,7 +1,7 @@
 import pygame, json
 
 class Button():
-	def __init__(self, x, y, image, scale):
+	def __init__(self, x, y, image, scale = 1):
 		width = image.get_width()
 		height = image.get_height()
 		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
@@ -10,25 +10,27 @@ class Button():
 		self.clicked = False
 
 	def draw(self, surface):
-		action = False
+		self.action = False
 
 		pos = pygame.mouse.get_pos()
 
-		if self.rect.collidepoint(pos):
-			if pygame.mouse.get_pressed()[0] and not self.clicked:
+		if self.rect.collidepoint(pos) and pygame.mouse.get_pressed()[0] and not self.clicked:
 				self.clicked = True
-				action = True
+				self.action = True
 
 		if pygame.mouse.get_pressed()[0] == 0:
 			self.clicked = False
 
 		surface.blit(self.image, (self.rect.x, self.rect.y))
 
-		return action
+		return self.action
+	
+	def act(self):
+		return self.action
 
 
 class Slider():
-	def __init__(self, x, y, valuetochange, bar_image, button_image, scale=1):
+	def __init__(self, x, y, bar_image, button_image, valuetochange = None, scale = 1):
 		self.x = x
 		self.y = y
 
@@ -37,7 +39,6 @@ class Slider():
 		self.button_width = int(button_image.get_width() * scale)
 		self.button_height = int(button_image.get_height() * scale)
 
-		self.scale = scale
 		self.value = 50
 		self.valuetochange = valuetochange
 		self.selected = False
@@ -47,8 +48,10 @@ class Slider():
 
 		self.bar_rect = self.bar_image.get_rect()
 		self.button_rect = self.button_image.get_rect()
+
 		self.bar_rect.center = (x,y)
 		self.button_rect.center = (x,y)
+
 		self.initial_center = (x,y)
 
 	
@@ -62,19 +65,69 @@ class Slider():
 		if self.selected and (not self.bar_rect.collidepoint(pygame.mouse.get_pos()) or not pygame.mouse.get_pressed()[0]):
 			self.selected = False
 			
-			with open("config.json", "r") as f:
-				data = json.load(f)
-			
-			data[self.valuetochange] = self.value
-			
-			with open("config.json", "w") as f:
-				json.dump(data, f)
+			if self.valuetochange != None:
+				with open("config.json", "r") as f:
+					data = json.load(f)
+				
+				data[self.valuetochange] = self.value
+				
+				with open("config.json", "w") as f:
+					json.dump(data, f)
 
 		if self.selected and self.initial_center[0] - self.bar_image.get_width() / 2 + self.button_image.get_width() / 2 < pygame.mouse.get_pos()[0] < self.initial_center[0] + self.bar_image.get_width() / 2 - self.button_image.get_width() / 2:
 			self.button_rect.center = (pygame.mouse.get_pos()[0], self.button_rect.center[1])
 			self.value = self.button_rect.center[0] - self.initial_center[0] + 50
 	
+	def value(self):
+		return self.value
+	
 		
+class CheckBox():
+	def __init__(self, x, y, unchecked_image, checked_image, scale = 1, valuetochange = None):
+		self.x = x
+		self.y = y
 
+		self.checked = False
+		self.valuetochange = valuetochange
+		self.clicked = False
+	
+		self.unchecked_width = int(unchecked_image.get_width() * scale)
+		self.unchecked_height = int(unchecked_image.get_height() * scale)
+		self.checked_width = int(checked_image.get_width() * scale)
+		self.checked_height = int(checked_image.get_height() * scale)
 
+		self.unchecked_image = pygame.transform.scale(unchecked_image, (self.unchecked_width, self.unchecked_height))
+		self.checked_image = pygame.transform.scale(checked_image, (self.checked_width, self.checked_height))
 
+		self.unchecked_rect = self.unchecked_image.get_rect()
+		self.checked_rect = self.checked_image.get_rect()
+
+		self.unchecked_rect.center = (x,y)
+		self.checked_rect.center = (x,y)
+
+		self.initial_center = (x,y)
+
+	def draw(self, surface):
+		if self.checked:
+			surface.blit(self.unchecked_image, (self.unchecked_rect.x, self.unchecked_rect.y))
+		else:
+			surface.blit(self.checked_image, (self.checked_rect.x, self.checked_rect.y))
+		
+		if self.unchecked_rect.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0] and not self.clicked:
+			self.checked = not self.checked
+			self.clicked = True
+
+			if self.valuetochange != None:
+				with open("config.json", "r") as f:
+					data = json.load(f)
+			
+				data[self.valuetochange] = self.checked
+			
+				with open("config.json", "w") as f:
+					json.dump(data, f)
+
+		if self.clicked and not pygame.mouse.get_pressed()[0]:
+			self.clicked = False
+
+	def ischecked(self):
+		return self.checked
